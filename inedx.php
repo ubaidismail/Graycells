6bcf45966fc461103882ee0935ee63b84517207f
ubaid ismail <ubaidismail378@gmail.com>
2020-05-28 00:18:24 +0500

    first commit

---
+++ inedx.php
@@ -0,0 +1,321 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<script>
+	$(document).ready(function(){
+    $("#click").click(function(){
+        $("#demo").toggle();
+    });
+    
+});
+</script>
+</head>
+		<body>
+				<div class="top-bar clearfix">
+					<div class="container">
+						<div class="social-follow">
+							<ul>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+							</ul>
+						</div>
+
+						<div class="admin">
+							<ul>
+								<li>
+									<a href="#"><i class="fa fa-lock"></i> Login</a>
+									<a href="#"><i class="fas fa-sign-in-alt"></i> Register</a>
+								</li>
+							</ul>
+						</div>
+					</div>					
+				</div><!-- top-bar -->
+				<header>
+					<div class="container">
+						<div class="row">
+							<div class="logo">
+								<img src="img/logo.jpg" alt="">
+							</div>
+							<nav>
+								<i class="fa fa-bars fa-3x checking" id="click"></i>
+								<ul id="demo">
+									<li><a href="#" class="current">Home</a></li>
+									<li><a href="#">About Us</a></li>
+									<li><a href="#">Serivce</a></li>
+									<li><a href="#">Shop</a></li>
+									<li><a href="#">Portfolio</a></li>
+									<li><a href="#">Pages</a></li>
+									<li><a href="#">Blog</a></li>
+									<li><a href="#">Contact Us</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</header>
+						<div class="slider">
+							<div class="container">
+								<h1><span>Graycells</span> Makes</h1>
+								<h2>Lead generation easy</h2>
+								<p>With its visual page builder, GrayCells revolutionsises the <br>way web professionals build great websites in no-time.</p>
+								<button><a href="#">Purchase now</a></button>
+							</div>
+						</div>
+
+
+						<section>
+							<div class="container">
+									<div class="row">
+										<div class="gray-cell">
+												<h2><span>Gray</span>CElls</h2>
+												<p>corporate template</p>
+												<img src="img/main.jpg" alt="">
+												<p class="ft-z">In the most amazing bootstrap 3 template with lot features that are  easily customizable and looks great in all devices. Loprem Ipsum dolor sit amel .consectetur <br> adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullomco Lorem ipsum <br> dolor sit amet ... </p>
+										</div><!-- gray cell-->
+												<div class="inner">
+													<div class="col-sm-4">
+														<img src="img/one-third-1.jpg" alt="">
+															<p>Sketch</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+															<p class="ft-x">View details</p>
+															<hr class="hr">
+													</div><!--col-->
+
+													<div class="col-sm-4">
+														<img src="img/one-third-2.jpg" alt="">
+															<p>Sketch</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+															<p class="ft-x">View details</p>
+															<hr class="hr">
+													</div><!--col-->
+
+													<div class="col-sm-4">
+														<img src="img/one-third-3.jpg" alt="">
+															<p>Sketch</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+															<p class="ft-x">View details</p>
+															<hr class="hr">
+													</div><!--col-->
+												</div>
+
+									</div>
+							</div>
+
+							<div class="bg-image">
+									<div class="container">
+										<div class="row">
+											<div class="check-out">
+												<h1>checkout our <span>services</span></h1>
+												<p><i>Provide the best service with high quality in every transaction desing is our commitment to conduct business sed ut perspicitatis unde omnis iste natus eroor sit <br>voluptetem accusantium doloremqkue laudatium totam rem apream tore veritatis et quasi beatae vitae dicta sunt explicabo.</i></p>
+											</div>
+											<div class="inner-2">
+												<div class="col-sm-4 rel">
+													<div class="check">
+														<i class="fa fa-lock fa-3x"></i>
+															<p>Graphics Design</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>
+															errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+													</div><!--2-->				
+												</div><!--col-->
+
+												<div class="col-sm-4 rel">
+													<div class="check">
+														<i class="fa fa-lock fa-3x"></i>
+															<p>Logo Design</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>
+															errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+													</div><!--2-->				
+												</div><!--col-->
+
+												<div class="col-sm-4 rel">
+													<div class="check">
+														<i class="fa fa-lock fa-3x"></i>
+															<p>Web Desing</p>
+															<p class="ft-z">Sed ut perspicatis unde omnis iste natus<br>
+															errot sit voluptatem accusantium<br>doloremque laudantium.</p>
+													</div><!--2-->				
+												</div><!--col-->
+											</div><!-- inner -->
+										</div>
+									</div>
+							</div><!--bg-image-->
+
+							<div class="gray-cell">
+												<p>More than 1000 free and premium high-quality design</p>
+												<img src="img/main.jpg" alt="">
+												<p class="ft-y">Lorem Ipsum dolor sit amel .consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis <br>nostrud exercitation ullomco Lorem ipsum  dolor sit amet ... </p>
+
+													<div class="btn-group">
+														<ul>
+															<li><a href="#" class="current">All</a></li>
+															<li><a href="#">Web</a></li>
+															<li><a href="#">Graphics</a></li>
+															<li><a href="#">Logo</a></li>
+															<li><a href="#">App</a></li>
+														</ul>
+													</div>
+							</div><!-- gray cell-->
+
+											<div class="inner-2">
+												<div class="container">
+													<div class="row">
+														<div class="col-sm-4">
+																<img src="img/ONE.gif" alt="" class="img-responsive">
+																<img src="img/2.gif" alt="" class="img-responsive">
+														</div><!--col--><div class="col-sm-4">
+																<img src="img/3.gif" alt="" class="img-responsive">
+																<img src="img/4.gif" alt="" class="img-responsive">
+														</div><!--col--><div class="col-sm-4">
+																<img src="img/5.gif" alt="" class="img-responsive">
+																<img src="img/6.gif" alt="" class="img-responsive">
+														</div><!--col-->
+													</div>
+												</div>
+											</div><!-- inner -->
+
+												<div class="counter-section">
+													<div class="container">
+														<div class="row">
+															<div class="col-sm-3">
+																<div class="counter">
+																	<i class="fa fa-cloud fa-3x"></i>
+																	<h1>5024</h1>
+																	<p>download</p>
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<i class="fa fa-user-circle fa-3x"></i>
+																	<h1>5024</h1>
+																	<p>download</p>
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<i class="fa fa-cloud fa-3x"></i>
+																	<h1>5024</h1>
+																	<p>download</p>
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<i class="fa fa-cloud fa-3x"></i>
+																	<h1>5024</h1>
+																	<p>download</p>
+																</div>
+															</div><!--col - lg -3-->
+														</div>
+													</div>
+												</div><!--counter sectin-->
+						<div class="our-video" style="background-color: #ededed; padding-bottom: 80px;">
+							<div class="container">
+								<div class="row">
+									<div class="gray-cell">
+												<h2><span>our</span>video</h2>
+												<p>We're professional agency with experience or more than decade</p>
+												<img src="img/check.png" alt="">
+														<div class="you-tube">
+															<div class="col-lg-6">
+																<iframe width="100%" height="315" src="https://www.youtube.com/embed/hff0p92I02M" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+															</div>
+															<div class="col-lg-6">
+																<p>
+																	Lorem ipum dolor sit amet consectetur adipiscing elit sed do eiusmod<br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim <br>veniam, quis nostrud  exercitation ullamaco. Lorem ipsum dolor sit amet
+																</p>
+																<div class="pera">
+																	<ul>
+																		<li><p>iPorro noster insolens at his at pri modo dolore iuvaret.</p></li>
+																		<li><p>iFacilis incider mea cu in quo dicit iudico.</p></li>
+																		<li><p>iVim solet denique delicatissimi ei ne essent insolens.</p></li>
+																	</ul>
+																</div>
+																<button class="add">View Details</button>
+															</div>
+														</div>
+													
+										</div><!-- gray cell-->
+								</div><!--row -->
+							</div>
+						</div>
+
+						</section>
+			
+						<footer>
+								<div class="counter-section  foot">
+													<div class="container">
+														<div class="row">
+															<div class="col-sm-3">
+																<div class="counter">
+																	<h3>About Gray Cells</h3>
+																	<p>Lorem ipsum dolor sit amet, consectetur <br>adipiscing elit. sed do eiusmod tempor<br> incidiunt ut labore et dolore magna <br>aliqua .Ut enim ad minim veniam, quies <br>nostrud exercitation ullamco.Lorem <br>ipsum dolor sit amet...</p>
+																	<div class="foot-social">
+																		<ul>
+																			<li><a href="#"><i class="fa fa-twitter fa-lg"></i></a></li>
+																			<li><a href="#"><i class="fa fa-facebook fa-lg" ></i></a></li>
+																			<li><a href="#"><i class="fa fa-phone fa-lg"></i></a></li>
+																			<li><a href="#"><i class="fa fa-linkedin fa-lg"></i></a></li>
+																		</ul>
+																	</div>
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<h3>quick links</h3>
+																 		<div class="links">
+																 			<ul>
+																 				<li>Our Services</li>
+																 				<li>Meet Our Team</li>
+																 				<li>Forum</li>
+																 				<li>Help Center</li>
+																 				<li>Contact Anakual</li>
+																 				<li>Privacy Policy</li>
+																 				<li>Anakual Team</li>
+																 				<li>Site Map</li>
+																 			</ul>
+																 		</div>	
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<h3>our newslatter</h3>
+																	<input type="text" placeholder="Your Name"><br>
+																	<input type="text" placeholder="Your Email"><br>
+																		<button class="adding">Submit Now</button>
+																</div>
+															</div><!--col - lg -3-->
+
+															<div class="col-sm-3">
+																<div class="counter">
+																	<h3>contact info</h3>
+																	<p><i class="fa fa-map-marker"></i> 28 Green Tower, Street Name <br>New york City USA</p>
+																	<p><i class="fa fa-phone"></i> Phone : +92 3480 205552</p>
+																	<p><i class="fa fa-envelope"></i> Email : Mutalqureshi953@gmail.com</p>
+																</div>
+															</div><!--col - lg -3-->
+
+															
+														</div><!-- row -->
+													</div><!-- col -->
+								</div><!--counter sectin-->						
+						</footer>
+
+		</body>
+</html>
